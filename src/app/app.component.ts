e45a4c57797f069eceaa3ef117b54d0eb23adc82
kryzanivska-nastya <154334074+nastya-kryhzanivskya@users.noreply.github.com>
2024-01-07 19:42:25 +0200

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'main-project';
+  title:any;
 }
+  
+
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[]= [
+{
+  id:1,
+  text: 'sth',
+  day:'august 5th',
+  reminder:true
+},
+{
+    id:2,
+    text: 'asd',
+    day:'november 5th',
+    reminder:true
+  },
+  {
+    id:3,
+    text: 'qwe',
+    day:'september 5th',
+    reminder:true
+  },
+]
